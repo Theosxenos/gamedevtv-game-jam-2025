39bd134807ab68b6dce43ab7acd844cb4547f7ca
Theosxenos <git.unpaid725@passfwd.com>
2025-05-21 19:08:17 +0200

    Refactor grid node. Convert to Area2D
    For easier use of a build area

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -4,8 +4,7 @@ class_name Grid extends Node2D
 @export var grid_color := Color(0.2, 0.2, 0.2)
 
 
-@onready var start := $Start.position as Vector2
-@onready var end := $End.position as Vector2 
+@onready var build_area: CollisionShape2D = $BuildArea
 
 
 func _draw() -> void:
@@ -13,6 +12,9 @@ func _draw() -> void:
 
 
 func _draw_grid() -> void:
+	var start: Vector2 = Vector2.ZERO
+	var end: Vector2 =  build_area.shape.size 
+	
 	for x in range(start.x, end.x + cell_size.x, cell_size.x):
 		draw_line(Vector2(x, start.y), Vector2(x, end.y), grid_color, 1)
 	for y in range(start.y, end.y + cell_size.y, cell_size.y):
