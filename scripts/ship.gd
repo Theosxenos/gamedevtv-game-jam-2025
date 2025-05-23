aaead7ff84e32ea17a5569e14fe75a285307b770
Theosxenos <git.unpaid725@passfwd.com>
2025-05-23 14:32:48 +0200

    Fix ship rotation animation
    Replace animation player with tween

--- scripts/ship.gd
+++ scripts/ship.gd
@@ -2,19 +2,20 @@ class_name Ship extends CharacterBody2D
 
 signal returning()
 
+enum States {MOVING, UNLOADING}
+
 @export var speed := 35.0
 @export var direction := Vector2.ZERO
 @export var goblin_scene : PackedScene = preload("uid://brlvn56mjcub")
 @export var can_unload := true
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _ready() -> void:
-	animation_player.get_animation("rotate").track_set_key_value(0, 0, rotation)
-	animation_player.get_animation("rotate").track_set_key_value(0, 1, rotation + PI)
+var current_state := States.MOVING
 
 
 func _physics_process(_delta: float) -> void:
+	if current_state != States.MOVING:
+		return
+	
 	move()
 	
 	
@@ -28,15 +29,17 @@ func move() -> void:
 
 
 func move_away() -> void:
-	animation_player.play("rotate")
-	direction *= -1
+	$CollisionShape2D.set_deferred("disabled", true)
+	await get_tree().process_frame
 	
-	await animation_player.animation_finished
-	
-	returning.emit()	
-	
+	var tween := create_tween()
+	tween.finished.connect(_on_tween_finished)
+	tween.tween_property(self, "rotation", rotation + PI, 1)
+
 		
 func unload() -> void:
+	current_state = States.UNLOADING
+	
 	var goblin: Goblin = goblin_scene.instantiate()
 	goblin.global_position = global_position + Vector2(0, 32)
 	get_parent().add_child(goblin)
@@ -47,3 +50,10 @@ func unload() -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_tween_finished() -> void:
+	direction *= -1
+	current_state = States.MOVING
+	
+	returning.emit()
