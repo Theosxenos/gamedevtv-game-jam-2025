ea8acb2191e2faa29cb8cb472f253767f1220c7b
Theosxenos <git.unpaid725@passfwd.com>
2025-05-24 15:00:57 +0200

    Add animations to goblin and warrior

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -4,6 +4,10 @@ class_name Goblin extends CharacterBody2D
 @export var direction := Vector2.ZERO
 
 
+func _ready() -> void:
+	$Sprite2D.flip_h = true if direction.x < 0 else false
+
+
 func _physics_process(_delta: float) -> void:
 	velocity = direction * speed
 	var collission = move_and_slide()
